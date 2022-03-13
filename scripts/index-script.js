3d64e312908e2c9bf9e84380261c97d1e7a57d4b
Michael Bateman <mikebateman@hotmail.co.uk>
2022-03-13 23:20:12 +0000

    Player buttons added

--- scripts/index-script.js
+++ scripts/index-script.js
@@ -1,1 +1,16 @@
-console.log("hello world");
+// Variables going to need for the game
+let playerTurn = 1;
+let ballPossessionTeam = 1;
+
+// Arrays for the game
+const pitchTiles = document.querySelectorAll(".football-pitch__tile");
+
+// Add listener to each of the football pitch tiles so that aware if one has been selected
+pitchTiles.forEach(e => {
+  e.addEventListener("click", tileSelected, false);
+});
+
+// TBC
+function tileSelected() {
+  
+}
